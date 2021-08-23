cda68d9ee9b35370e3598c0142c0e5dd9fbd3afe
HarikrishnaMogan <harikrishss437@gmail.com>
2021-08-23 23:18:30 +0530

    changed all

--- script.js
+++ script.js
@@ -3,23 +3,39 @@ function api(countries)
 {
     fetch(`https://api.covid19api.com/live/country/${countries}`)
     .then(data=>data.json())
-    .then(count=> displaycount(count));
+    .then(count=> displaycount(count ,countries))
+    .catch(err=>console.log(err));
     container.innerHTML="";
 }
 api("India");
 
 //for displaying countries data
-function displaycount(count)
+function displaycount(count,con)
 {
     let card = document.createElement("div");
-    card.innerHTML=`
-    <h1 class="cases">Cases:${count[count.length-1].Confirmed}</h1>
-    <h2 class="Deaths">Deaths:${count[count.length-1].Deaths}</h2>
-    `;
-    container.append(card);
+    card.className="card";
+    if(count.length==0)
+    {
+        card.innerHTML=`<h2>Country is wrong</h2>`;
+        container.append(card);
+    }
+    else{
+        card.innerHTML=`
+        <h1 class="cases">${con}</h1>
+        <h1 class="cases">Cases:${count[count.length-1].Confirmed}</h1>
+        <h2 class="Deaths">Deaths:${count[count.length-1].Deaths}</h2>
+        `;
+        container.append(card);
+    }
+   
+   
 }
 
-
+function search()
+{
+    let country = document.querySelector(".input").value;
+    api(country);
+}
 //for menu animations
 function burgertoggle()
 {
